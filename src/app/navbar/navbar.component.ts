98c7c5c748582b83b7c5ce8fe63ba73b7f83e2ef
IronKing24 <manarsaad45@gmail.com>
2023-03-24 22:57:31 +0300

    rework animation wip

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,47 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import {Component} from '@angular/core';
 
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
-  
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.scss'],
+	animations: [
+		trigger('openClosePane',[
+			state("closed", style({ transform: "translateX(-90 %)"})),
+			state("open", style({ transform: "translateX(0 %)" })),
+			transition("open <=> closed", animate("0.06s ease-in-out"))
+		])
+	]
 })
-export class NavbarComponent  {
-  public navbarShow: boolean = false;
+export class NavbarComponent {
+	public visible: boolean = false;
+	public symbol: string = "≡";
+
+	onBtnClick()
+	{
+		this.visible = !this.visible;
+		this.symbol = (this.visible) ? "x" : "≡";
+	}
 }
 
-
+animations: [
+	trigger('openClose', [
+		state('open', style({
+			height: '200px',
+			opacity: 1,
+			backgroundColor: 'yellow'
+		})),
+		state('closed', style({
+			height: '100px',
+			opacity: 0.8,
+			backgroundColor: 'blue'
+		})),
+		transition('open => closed', [
+			animate('1s')
+		]),
+		transition('closed => open', [
+			animate('0.5s')
+		]),
+	]),
+]
