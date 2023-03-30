5551950856f6d2f13c8c0449835d5b8589c299dc
IronKing24 <manarsaad45@gmail.com>
2023-03-30 18:35:16 +0300

    implement d3

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,23 +1,49 @@
-import { Component, AfterViewInit, ViewChildren, Input } from '@angular/core';
+import { Component, OnInit , AfterViewInit, ViewChildren, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { svg, select } from 'd3';
 
 @Component({
 	selector: 'app-map',
-	templateUrl: './map.component.svg',
+	templateUrl: './map.component.html',
 	styleUrls: ["map.component.scss"]
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements OnInit, AfterViewInit {
 
-	@Input() location:string | undefined;
-	petra = "petra"
-	constructor(private router: Router)
+	svgHTML: SafeHtml | undefined;
+
+	constructor(
+		private router: Router,
+		private sanitizer: DomSanitizer
+		)
 	{
 	}
 
-	ngAfterViewInit(): void { 
+	async ngOnInit()
+	{
+		try {
+			await svg("assets/images/map.svg")
+				.then(
+					(svgFile) => {
+						const svg = this.sanitizer.bypassSecurityTrustHtml(svgFile.documentElement.innerHTML)
+						this.svgHTML = svg;
+					},
+					() => {
+						console.warn("Wasn't able to load map SVG.");
+					}
+				);
+		} catch (error) {
+			console.warn(error)
+		}
+	}
+
+	ngAfterViewInit(){
+
+		
+
 		let pathList = document.querySelectorAll("path")
-		//let nameGroup = document.getElementById("text")
-		//let mapSvg = document.getElementById("map")
+		let nameGroup = document.getElementById("text")
+		let mapSvg = document.getElementById("map")
 
 		pathList?.forEach(element => {
 			element.addEventListener("mouseover", function () {
@@ -29,7 +55,7 @@ export class MapComponent implements AfterViewInit {
 			element.addEventListener('click', function (){
 				element.style.fill = "green";
 				
-				//nameGroup?.setAttribute("visibility", "hidden");
+				nameGroup?.setAttribute("visibility", "hidden");
 				//pathList?.forEach(path => {
 				//	if(element !== path)
 				//	{
